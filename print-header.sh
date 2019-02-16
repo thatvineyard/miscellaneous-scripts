f41a84d03a97954e651de86b28fe0e02353ed1cf
Carl Wingårdh <c.a.wingardh@gmail.com>
2019-02-16 19:14:01 +0100

    Create header printing script

---
+++ print-header.sh
@@ -0,0 +1,151 @@
+#!/bin/bash
+
+
+topline="================ Error =================\n"
+startmainline="\t"
+endmainline=""
+
+
+
+messagewidth=40
+terminalwidth=60
+linespacing=2
+character=\=
+header=Message
+
+strU8DiffLen () {
+    local bytlen oLang=$LANG
+    oLcAll=$LC_ALL
+    LANG=C
+    LC_ALL=C
+    bytlen=${#1}
+    LANG=$oLang LC_ALL=$oLcAll
+    return $(( bytlen - ${#1} ))
+}
+
+function print_top {
+    messagelength=$(echo $1 | wc -c)
+    marginalperside=2
+
+    offset=$(((messagelength + marginalperside + $3 + 1) % 2))
+    leftsidelength=$((($3 / 2) - ($messagelength / 2) - $marginalperside))
+    rightsidelength=$((($3 / 2) - ($messagelength / 2) - $marginalperside + $offset))
+
+    
+    eval printf "%0.s$2" {1..$leftsidelength}
+    eval printf "%0.s' '" {1..$marginalperside}
+    printf $1
+    eval printf "%0.s' '" {1..$marginalperside}
+    eval printf "%0.s$2" {1..$rightsidelength}
+    printf "\n"
+    for i in $(seq 1 $linespacing); do
+	printf "\n"
+    done
+}
+
+function print_tail {
+    for i in $(seq 1 $linespacing); do
+	printf "\n"
+    done
+
+    eval printf "%0.s$1" {1..$2}
+    printf "\n"
+}
+
+function print_main {
+  
+    if [[ $terminalwidth -gt $messagewidth ]]; then 
+	prefixlength=$((($terminalwidth - $messagewidth) / 2))
+	prefix=$(eval printf "%0.s' '" {1..$prefixlength})
+    else
+	prefix=''
+    fi
+    
+    fold -s --width $messagewidth <<< $@ |  sed "s/^/$prefix/"
+}
+
+function print_header {
+    character=$1
+    shift
+    messagewidth=$1
+    shift
+    terminalwidth=$1
+    shift
+    header=$1
+    shift
+    
+    print_top $header $character $terminalwidth
+    print_main $@
+    print_tail $character $terminalwidth
+}
+
+
+while test $# -gt 0; do
+    case "$1" in
+	-h|--help)
+	    echo "$package - attempt to capture frames"
+	    echo " "
+	    echo "$package [options] application [arguments]"
+	    echo " "
+	    echo "options:"
+	    echo "-h, --help                show brief help"
+	    echo "-a, --action=ACTION       specify an action to use"
+	    echo "-o, --output-dir=DIR      specify a directory to store output in"
+	    exit 0
+	    ;;
+	-H)
+	    shift
+	    if test $# -gt 0; then
+		header=$1
+	    else
+		echo "Header required after '-H' flag"
+		exit 1
+	    fi
+	    shift
+	    ;;
+	-l)
+	    shift
+	    if test $# -gt 0; then
+		linespacing=$1
+	    else
+		echo "Line spacing required after '-l' flag"
+		exit 1
+	    fi
+	    shift
+	    ;;
+	-w)
+	    shift
+	    if test $# -gt 0; then
+		messagewidth=$1
+	    else
+		echo "Message width required after '-w' flag"
+		exit 1
+	    fi
+	    shift
+	    ;;
+	-t)
+	    shift
+	    if test $# -gt 0; then
+		terminalwidth=$1
+	    else
+		echo "Terminal width required after '-t' flag"
+		exit 1
+	    fi
+	    shift
+	    ;;
+	-c)
+	    shift
+	    if test $# -gt 0; then
+		character=$1
+	    else
+		echo "Chacter required after '-c' flag"
+		exit 1
+	    fi
+	    shift
+	    ;;
+	*)
+	    print_header $character $messagewidth $terminalwidth $header $@
+	    break
+	    ;;
+    esac
+done
