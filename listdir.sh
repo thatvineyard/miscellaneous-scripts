6b4c55a2418dc072082c27f646fbeb235b2ccfc0
Carl Wingårdh <c.a.wingardh@gmail.com>
2019-03-14 23:43:26 +0100

    Now only looks at dirs, also added cd-ing functionality, but requires user to source the script

--- listdir.sh
+++ listdir.sh
@@ -2,7 +2,7 @@
 
 number=1
 
-ls $1 | while read line ; do
+ls -1d $1/*/ | while read line ; do
     if [[ "$line" =~ "$2" ]] ; then
 	echo "$number: $line"
 	number=$((number+1))
@@ -10,16 +10,13 @@ ls $1 | while read line ; do
 done
 
 number=1
+dir=""
 
-ls $1 | while read line ; do
+ls -1d $1/*/ | while read line ; do
     if [[ "$line" =~ "$2" ]] ; then
 	if [[ $number == $3 ]] ; then
-	    dir="$1$dir/$line"
-	    echo $dir
-	    break
+	    cd $line
 	fi
 	number=$((number+1))
     fi
 done
-
-echo $dir
